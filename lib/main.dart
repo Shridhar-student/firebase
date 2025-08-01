13b3b7e1080d1200406c3e41993d16faaaced42c
Shridhar <shridhar.chavan@centvis.com>
2025-08-01 15:29:25 +0530

    fix:test

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,6 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const CardScreen());
+        home: const SplashScreen());
   }
 }
